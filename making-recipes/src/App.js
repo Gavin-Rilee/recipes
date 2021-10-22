df85b4038fde700b011ff0f0837c541abff98ac7
Gavin R <grilee12@gmail.com>
2021-10-22 17:12:24 -0400

    Done with product just need to style

--- making-recipes/src/App.js
+++ making-recipes/src/App.js
@@ -1,35 +1,61 @@
-import './App.css';
-import React, { useEffect,useState } from 'react';
-import Recipe from './Recipe';
-
+import "./App.css";
+import React, { useEffect, useState } from "react";
+import Recipe from "./Recipe";
 
 function App() {
+  const APP_ID = "6a82bf5a";
+  const APP_KEY = "41c824e6c0e2508b4cf5a44926fa0205";
 
-  const APP_ID = '6a82bf5a'
-  const APP_KEY = '41c824e6c0e2508b4cf5a44926fa0205'
-
-  const [recipes, setRecipes] = useState([])
+  const [recipes, setRecipes] = useState([]);
+  const [search, setSearch] = useState("");
+  const [query, setQuery] = useState("chicken");
 
   useEffect(() => {
     getRecipes();
-  }, [])
+  }, [query]);
 
-  const getRecipes = async () =>{
-    const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
-    const data =await response.json();
+  const getRecipes = async () => {
+    const response = await fetch(
+      `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`
+    );
+    const data = await response.json();
     setRecipes(data.hits);
-  }
+  };
+
+  const updateSearch = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const getSearch = (e) => {
+    e.preventDefault();
+    setQuery(search);
+    setSearch("");
+  };
 
   return (
     <div className="App">
-      <form className="search-form">
-        <input className="search-bar" type="text"/>
-        <button className="search-button"type="submit">Search</button>
-        
+      <form onSubmit={getSearch} className="search-form">
+        <input
+          className="search-bar"
+          type="text"
+          value={search}
+          onChange={updateSearch}
+        />
+        <button className="search-button" type="submit">
+          Search
+        </button>
       </form>
-        {recipes.map(recipe => (
-          <Recipe/>
-        ))}
+      <div className="recipes">
+      {recipes.map((recipe) => (
+        <Recipe
+          key={recipe.recipe.label}
+          title={recipe.recipe.label}
+          calories={recipe.recipe.calories}
+          image={recipe.recipe.image}
+          ingredients={recipe.recipe.ingredients}
+        />
+      ))}
+      </div>
     </div>
   );
 }
